a43eaf30e6aee87983dc2fc95132fd9204d4ef3b
jsamperevazquez <jsamperevazquez@danielcastelao.org>
2021-11-11 02:09:36 +0100

    Primera ventana

--- ejercicios/interfaces/Interface.py
+++ ejercicios/interfaces/Interface.py
@@ -1,10 +1,9 @@
 import gi
+
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
 ventana = Gtk.Window()
 ventana.show()
-ventana.connect("destroy",Gtk.main_quit)
+ventana.connect("destroy", Gtk.main_quit)
 Gtk.main()
-
-
